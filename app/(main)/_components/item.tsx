038f0f1f44be5591f09a33758d627bb6f2241509
yhyf/meihao <meihao617@dingtalk.com>
2023-10-18 17:15:10 +0800

    feat：Document create ant delete controls

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,11 +1,26 @@
 "use client"
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 import { Skeleton } from "@/components/ui/skeleton"
 import { api } from "@/convex/_generated/api"
 import { Id } from "@/convex/_generated/dataModel"
 import { cn } from "@/lib/utils"
+import { useUser } from "@clerk/clerk-react"
 import { useMutation } from "convex/react"
-import { ChevronDown, ChevronRight, LucideIcon, Plus } from "lucide-react"
+import {
+  ChevronDown,
+  ChevronRight,
+  LucideIcon,
+  MoreHorizontal,
+  Plus,
+  Trash,
+} from "lucide-react"
 import { useRouter } from "next/navigation"
 import React from "react"
 import { toast } from "sonner"
@@ -35,8 +50,12 @@ const Item = ({
   level,
   onExpand,
 }: ItemProps) => {
+  const { user } = useUser()
   const router = useRouter()
+
   const create = useMutation(api.documents.create)
+  const archive = useMutation(api.documents.archive)
+
   const ChevronIcon = expanded ? ChevronDown : ChevronRight
 
   const handleExpand = (
@@ -55,7 +74,7 @@ const Item = ({
         if (!expanded) {
           onExpand?.()
         }
-        router.push(`/documents/${documentId}`)
+        // router.push(`/documents/${documentId}`)
       }
     )
 
@@ -63,6 +82,19 @@ const Item = ({
       loading: "Creating a new note...",
       success: "New note created!",
       error: "Failed to create a new note.",
+    })
+  }
+
+  const onArchive = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    event.stopPropagation()
+    if (!id) return
+
+    const promise = archive({ id })
+
+    toast.promise(promise, {
+      loading: "Moving to trash...",
+      success: "Note moved to trash!",
+      error: "Failed to archive note.",
     })
   }
 
@@ -98,6 +130,31 @@ const Item = ({
       )}
       {!!id && (
         <div className="ml-auto flex items-center gap-x-2">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild onClick={(e) => e.stopPropagation()}>
+              <div
+                role="button"
+                className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600"
+              >
+                <MoreHorizontal className="h-4 w-4 text-muted-foreground" />
+              </div>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+              className="w-60"
+              align="start"
+              side="right"
+              forceMount
+            >
+              <DropdownMenuItem onClick={onArchive}>
+                <Trash className="h-4 w-4 mr-2" />
+                Delete
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <div className="text-xs text-muted-foreground p-2">
+                Last edited by: {user?.fullName}
+              </div>
+            </DropdownMenuContent>
+          </DropdownMenu>
           <div
             role="button"
             onClick={onCreate}
