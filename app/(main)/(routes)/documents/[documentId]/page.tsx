c6be2917753340f50bd93df0cd8058bfedcfbf59
Mona Lisa <1587945736@qq.com>
2023-10-25 22:21:28 +0800

    feat：create picker-icon and title component

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,5 +1,37 @@
-const DocumentIdPage = () => {
-  return <div>DocumentIdPage</div>
+"use client"
+
+import Toolbar from "@/app/(main)/_components/toolbar"
+import { api } from "@/convex/_generated/api"
+import { Id } from "@/convex/_generated/dataModel"
+import { useQuery } from "convex/react"
+
+interface DocumentIdPageProps {
+  params: {
+    documentId: Id<"documents">
+  }
+}
+
+const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId,
+  })
+
+  if (document === void 0) {
+    return <div>Loading...</div>
+  }
+
+  if (document === null) {
+    return <div>Not found</div>
+  }
+
+  return (
+    <div className="pb-40">
+      <div className="h-[35vh]"></div>
+      <div className="md:max-w-3xl lg:md-max-w-4xl mx-auto">
+        <Toolbar initialData={document} />
+      </div>
+    </div>
+  )
 }
 
 export default DocumentIdPage
